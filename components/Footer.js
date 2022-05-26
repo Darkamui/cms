b5f59469903d77fb69ac259aaf1d02cd684d9c10
Daniel Jerusalmy <dannyerushalmi@gmail.com>
2022-05-26 14:10:56 -0400

    footer add links

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 const Footer = () => {
 	return (
 		<div className="footer-section">
-			<div className="fb-logo">
-				<Image src="/fb.webp" width="50px" height="50" />
+			<div className="footer-links">
+				<Link to href="/sites">
+					Liste Des Parois
+				</Link>
+				<div className="fb-logo">
+					<Image src="/fb.webp" width="50px" height="50" />
+				</div>
+				<a href="https://fqme.qc.ca/adherer/">Adhérer FQME</a>
 			</div>
 			<p>
 				© 2022 Club de Montagne du Saguenay. Conception originale Catherine
